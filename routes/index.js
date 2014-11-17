3a4bca50a982ee3d58a3163e36f22f27f9c2b9f0
mrganser <gersuarod@gmail.com>
2014-11-17 20:32:47 +0000

    bootstrapped

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Magic Box' });
 });
 
 module.exports = router;
